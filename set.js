6b6f939b5b3187a7a65a44645792105abafbc271
Ianmuhandichi <159758557+Ianmuhandichi@users.noreply.github.com>
2025-08-10 12:46:10 +0300

    Update set.js

--- set.js
+++ set.js
@@ -19,18 +19,18 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 
 const config = {
     session: process.env.SESSION_ID || '', //paste your session here 
-    PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Keith", //replace with your owner name
-    NUMERO_OWNER: process.env.NUMERO_OWNER || "254748387615",   //replace with your owner number  
+    PREFIXE: process.env.PREFIX || "*",
+    OWNER_NAME: process.env.OWNER_NAME || "IAN TECH", //replace with your owner name
+    NUMERO_OWNER: process.env.NUMERO_OWNER || "254723278526",   //replace with your owner number  
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',    
     URL: process.env.URL || "https://files.catbox.moe/4h8lfw.jpg",    //replace with your image url                     
     ANTICALL_MSG: process.env.ANTICALL_MSG || 'call declined',             
-    GURL: process.env.GURL || "https://github.com/Keithkeizzah", // replace with your url
+    GURL: process.env.GURL || "https://github.com/Ianmuhandichi/ALPHA-MDR", // replace with your url
     EVENTS: process.env.EVENTS || "yes",    
-    BOT: process.env.BOT_NAME || 'ALPHA-MD', //replace with your bot name
+    BOT: process.env.BOT_NAME || 'IAN TECH', //replace with your bot name
     MODE: process.env.PUBLIC_MODE || "yes",              
     TIMEZONE: process.env.TIMEZONE || "Africa/Nairobi", //replace with your timezone 
-    DP: process.env.STARTING_BOT_MESSAGE || "yes",
+    DP: process.env.STARTING_BOT_MESSAGE || "no",
     ADM: process.env.ANTI_DELETE_MESSAGE || 'yes',
     
     DATABASE_URL,
